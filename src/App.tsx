7f8a2d444f9b694b7bd4b534a1518361d2dccd01
Tanvir Hossain <tanvir.hossain303030@gmail.com>
2025-06-25 21:55:55 +0600

    implement action

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
+import { useDispatch } from "react-redux"
+import { decrement, increment } from "./redux/features/counter/counterSlice";
 
 
 function App() {
+  const dispatch = useDispatch();
 
+  const handelIncrement = () => {
+    dispatch(increment())
+  };
+
+  const handelDecrement = () => {
+    dispatch(decrement())
+  }
 
   return (
     <div>
       <h1>Counter With Redux</h1>
-      <button>Increment </button>
+      <button onClick={handelIncrement}>Increment </button>
       <div>0</div>
-      <button>Decrement</button>
+      <button onClick={handelDecrement}>Decrement</button>
       <div>0</div>
     </div>
   )
