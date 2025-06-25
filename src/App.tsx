b4914050e5c92881bd053165b681a02c8becf051
Tanvir Hossain <tanvir.hossain303030@gmail.com>
2025-06-25 22:32:02 +0600

    implement payload

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useDispatch } from "react-redux"
+
 import { decrement, increment } from "./redux/features/counter/counterSlice";
 import { useAppDispatch, useAppSelector } from "./redux/hook";
 
@@ -7,8 +7,8 @@ function App() {
   const dispatch = useAppDispatch();
   const { count } = useAppSelector((state) => state.counter);
 
-  const handelIncrement = () => {
-    dispatch(increment())
+  const handelIncrement = (amount: number) => {
+    dispatch(increment(amount))
   };
 
   const handelDecrement = () => {
@@ -18,7 +18,8 @@ function App() {
   return (
     <div>
       <h1>Counter With Redux</h1>
-      <button onClick={handelIncrement}>Increment </button>
+      <button onClick={() => handelIncrement(5)}>Increment By 5 </button>
+      <button onClick={() => handelIncrement(1)}>Increment By 1 </button>
       <div>{count}</div>
       <button onClick={handelDecrement}>Decrement</button>
     </div>
